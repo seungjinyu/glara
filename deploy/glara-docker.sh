ff1344293381903f4295ce33f809fc6cb380cff3
whitegoblin <seungjinyu93@gmail.com>
2022-03-16 17:29:13 +0900

    [FIX] fixed the panic to log and configured the deployment for kubernetes

--- deploy/glara-docker.sh
+++ deploy/glara-docker.sh
@@ -1,5 +1,5 @@
         kubectl create secret docker-registry registry-secret --namespace=glara \
-        --docker-server=registry.clever.vsmart00.com \
+        --docker-server=registry.egypt.vsmart00.com \
         --docker-username=haruband \
         --docker-password=kk3249 \
         --docker-email=$haruband@abc.com
